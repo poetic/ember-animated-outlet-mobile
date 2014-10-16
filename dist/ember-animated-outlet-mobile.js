895efa915d5aeab59f0bc72f82b300c62c7647be
Jake Craige <jake@poeticsystems.com>
2014-10-16 07:09:24 -0500

    dist build

--- dist/ember-animated-outlet-mobile.js
+++ dist/ember-animated-outlet-mobile.js
@@ -1,6 +1,6 @@
 /**
   Write me...
- 
+
   @class AnimatedContainerView
   @namespace Ember
   @extends Ember.ContainerView
@@ -8,14 +8,14 @@
 Ember.AnimatedContainerView = Ember.ContainerView.extend({
 
     classNames: ['ember-animated-container'],
-    
+
     init: function() {
         this._super();
         //Register this view, so queued effects can be related with this view by name
         Ember.AnimatedContainerView._views[this.get('name')] = this;
         this._isAnimating = false;
     },
-    
+
     willDestroy: function() {
         this._super();
         //Clean up
@@ -23,7 +23,7 @@ Ember.AnimatedContainerView = Ember.ContainerView.extend({
         delete Ember.AnimatedContainerView._views[name];
         delete Ember.AnimatedContainerView._animationQueue[name];
     },
-    
+
     //Override parent method
     _currentViewWillChange: Ember.beforeObserver(function() {
         var currentView = Ember.get(this, 'currentView');
@@ -108,7 +108,7 @@ Ember.AnimatedContainerView = Ember.ContainerView.extend({
     enqueueAnimation: function(effect) {
         Ember.AnimatedContainerView._animationQueue[this.get('name')] = effect;
     },
-    
+
     setCurrentViewAnimated: function(currentView, effect) {
         this.enqueueAnimation(effect);
         this.set('currentView', currentView);
@@ -117,10 +117,10 @@ Ember.AnimatedContainerView = Ember.ContainerView.extend({
 });
 
 Ember.AnimatedContainerView.reopenClass({
-    
+
     /**
       All animated outlets registers itself in this hash
-       
+
       @private
       @property {Object} _views
     */
@@ -136,10 +136,15 @@ Ember.AnimatedContainerView.reopenClass({
 
     /**
       Enqueue effects to be executed by the given outlets when the next route transition happens.
-      
+
       @param {Object} animations A hash with keys corresponding to outlet views and values with the desired animation effect.
     */
     enqueueAnimations: function(animations) {
+        // Animations cause race conditions in testing so we just disable them
+        if (Ember.testing) {
+          return;
+        }
+
         for (var name in animations) {
             if (!animations.hasOwnProperty(name)) continue;
             this._animationQueue[name] = animations[name];
@@ -157,9 +162,9 @@ Ember.AnimatedContainerView.reopenClass({
 
     /**
       Register a new effect.
-     
+
       The `callback` function will be passed the following parameters:
-     
+
       - The `Ember.AnimatedContainerView` instance.
       - The new view.
       - The old view.
@@ -541,227 +546,3 @@ Ember.AnimatedContainerView.registerEffect('slideOverDown', function(ct, newView
 });
 
 })();
-
-/*
- https://github.com/JamesHight/emberjs-touch
-
- Copyright (c) 2012 by:
-
- * James Hight
- *
- * Update to support ember-animated-outlet by Jake Craige 2013
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-
-(function(){
-  var touch = {
-    start: false, // has the touchstart event been triggered and is it still a valid click event?
-    // starting coordinates of touch event
-    x: null,
-    y: null,
-    enabled: true
-  };
-
-  var deviceIsAndroid = navigator.userAgent.indexOf('Android') > 0;
-  var deviceIsIOS = /iP(ad|hone|od)/.test(navigator.userAgent);
-
-  var fastClick = {
-    findControl: function(labelElement) {
-        if (labelElement.control !== undefined) {
-          return labelElement.control;
-        }
-
-        // All browsers under test that support touch events also support the HTML5 htmlFor attribute
-        if (labelElement.htmlFor) {
-          return document.getElementById(labelElement.htmlFor);
-        }
-
-        // If no for attribute exists, attempt to retrieve the first labellable descendant element
-        // the list of which is defined here: http://www.w3.org/TR/html5/forms.html#category-label
-        return labelElement.querySelector('button, input:not([type=hidden]), keygen, meter, output, progress, select, textarea');
-    },
-    needsFocus: function(target) {
-      switch (target.nodeName.toLowerCase()) {
-        case 'textarea':
-          return true;
-        case 'select':
-          return !deviceIsAndroid;
-        case 'input':
-          switch (target.type) {
-          case 'button':
-          case 'checkbox':
-          case 'file':
-          case 'image':
-          case 'radio':
-          case 'submit':
-            return false;
-          }
-
-          // No point in attempting to focus disabled inputs
-          return !target.disabled && !target.readOnly;
-        default:
-          return (/\bneedsfocus\b/).test(target.className);
-        }
-    }
-  }
-
-
-  // Temporarily disable touch to prevent duplicate clicks
-  function disableTouch() {
-    if (touch.enabled) {
-      touch.enabled = false;
-      setTimeout(function() {
-        touch.enabled = true;
-      }, 400);
-    }
-  }
-
-  Ember.EventDispatcher.reopen({
-    setupHandler: function(rootElement, event, eventName) {
-      var self = this,
-        moved;
-
-      if (!touch.enabled) return;
-
-
-      var touchHandler = function(evt, triggeringManager) {
-        // Track touch events to see how far the user's finger has moved
-        // If it is > 20 it will not trigger a click event
-
-        switch(evt.type) {
-          // Remember our starting point
-          case 'touchstart':
-            touch.start = true;
-            touch.x = evt.originalEvent.touches[0].clientX;
-            touch.y = evt.originalEvent.touches[0].clientY;
-            evt.stopPropagation();
-            break;
-
-          // Monitor touchmove in case the user moves their finger away and then back to the original starting point
-          case 'touchmove':
-            if (touch.start) {
-              moved = Math.max(Math.abs(evt.originalEvent.touches[0].clientX - touch.x),
-                      Math.abs(evt.originalEvent.touches[0].clientY - touch.y));
-              if (moved > 20)
-                touch.start = false;
-            }
-            break;
-
-          // Check end point
-          case 'touchend':
-            if (touch.start) {
-              moved = Math.max(Math.abs(evt.originalEvent.changedTouches[0].clientX - touch.x),
-                    Math.abs(evt.originalEvent.changedTouches[0].clientY - touch.y));
-
-              var $target = $(evt.target);
-
-              if(deviceIsAndroid && $target.is(':input,select')) {
-                // Android fix for input's and selects
-              } else if (moved < 20) {
-                evt.preventDefault();
-                evt.stopImmediatePropagation();
-                // All tests have passed, trigger click event
-                if (touch.enabled) {
-
-                  var control
-                  var type    = $target.attr('type');
-                  var tagName = evt.target.tagName.toLowerCase()
-
-                  if(tagName === 'label') {
-                    control = $(fastClick.findControl(evt.target));
-                    if(control.attr('type') == 'checkbox') {
-                      control.prop('checked', !control.prop('checked')).change()
-                    } else {
-                      $(control).focus()
-                    }
-
-                  } else if(type == 'checkbox') {
-                    $target.prop('checked', !$target.prop('checked')).change()
-
-                  } else if (!$target.hasClass('needsclick') && fastClick.needsFocus(evt.target)){
-                      if (deviceIsIOS && evt.target.setSelectionRange && evt.target.type.indexOf('date') !== 0 && evt.target.type !== 'time') {
-                        length = evt.target.value.length;
-                        evt.target.setSelectionRange(length, length);
-                      } else {
-                        evt.target.focus();
-                      }
-
-                  } else {
-                    if (document.activeElement && document.activeElement !== evt.target) {
-                      document.activeElement.blur();
-                    }
-
-                    $target.click();
-                  }
-
-                }
-              }
-              touch.start = false;
-            }
-            break;
-        }
-
-        // END touch code
-
-        var view = Ember.View.views[this.id],
-        result = true, manager = null;
-
-        manager = self._findNearestEventManager(view,eventName);
-
-        if (manager && manager !== triggeringManager) {
-          if (eventName == 'click') {
-            if (touch.enabled)
-              disableTouch();
-            else
-              return false;
-          }
-          result = self._dispatchEvent(manager, evt, eventName, view);
-        } else if (view) {
-          result = self._bubbleEvent(view,evt,eventName);
-        } else {
-          evt.stopPropagation();
-        }
-
-        return result;
-      }
-
-      rootElement.delegate('.ember-view', event + '.ember', function(evt, triggeringManager) {
-        return touchHandler.call(this, evt, triggeringManager)
-      });
-
-      rootElement.delegate('[data-ember-action]', event + '.ember', function(evt) {
-        var actionId = Ember.$(evt.currentTarget).attr('data-ember-action'),
-          action   = Ember.Handlebars.ActionHelper.registeredActions[actionId],
-          handler  = null;
-
-        if (action && action.handler && action.eventName === eventName) {
-          handler = action.handler;
-
-          if (touch.enabled)
-            disableTouch();
-          else
-            return false;
-
-          return handler(evt);
-        }
-      });
-
-      rootElement.delegate('.animated-link-view', event + '.ember', function(evt, triggeringManager) {
-        return touchHandler.call(this, evt, triggeringManager)
-      });
-    }
-  });
-})();
-
